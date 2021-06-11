02f110d7abfddb13a94851ccba84461cbb4bb345
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2021-06-11 15:28:46 +1000

    more changes to mobile styling

--- src/components/PlaceTile.js
+++ src/components/PlaceTile.js
@@ -5,23 +5,23 @@ const PlaceTile = ({ placeObj, getPlaceDetails }) => {
 
     return (
         <div className="my-4 mx-2 rounded bg-highlightHigh px-2 py-1">
-            <div className="">
+            
                 <h1 className="text-2xl">{placeObj.name}</h1>
                 
-                <div className="flex justify-between items-center">
+                <div className="flex lg:justify-between lg:items-center lg:flex-row flex-col">
                     <Rating 
                     name="stars"
                     value={placeObj.rating}
                     readOnly
                     precision={0.1}
                     />
-                <button 
-                    className=" p-2 border-2 border-black rounded"
-                    onClick={() => getPlaceDetails(placeObj.place_id)}
-                > 
-                    More Details
-                </button>
-                </div>
+                    <button 
+                        className=" p-2 border-2 border-black rounded"
+                        onClick={() => getPlaceDetails(placeObj.place_id)}
+                    > 
+                        More Details
+                    </button>
+               
                 
             </div>
             
